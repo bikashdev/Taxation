310a6e6d4a7e2d4f8536e1b15308a4b52fb57a39
BikashGosai <bikashgosai@hotmail.com>
2019-02-08 14:43:45 +0545

    added to git directory

---
+++ Taxation/Taxation/Calculate.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using System;
+using System.Globalization;
+
+namespace core
+{
+    public static class Calculator
+    {
+        public static string Calculate(string monthly1, bool isMarried, bool isBonus=false, string bonus="")
+        {
+            //According to fiscal year 2074/075;
+            decimal firstLimitUn = 350000;
+            decimal firstLimitMa = 400000;
+            decimal secondLimitUn = 450000;
+            decimal secondLimitMa = 500000;
+            //decimal thirdLimitUn = 550000;
+            //decimal thirdLimitMa = 600000;
+            decimal monthly = Decimal.Parse(monthly1);
+            decimal yearlyTax = 0M;
+            decimal monthlyTax = 0M;
+            decimal monthlyAfterTaxDeduction = 0M;
+            decimal remainingAmount = 0M;
+            decimal secondRemainingAmount = 0M;
+
+            if (monthly!=0.00M ||  monthly.ToString() != String.Empty)
+            {
+                decimal yearly = monthly * 12;
+                if (isMarried)
+                {
+                    if (yearly > 0 && yearly <= firstLimitMa)
+                    {
+                        yearlyTax = yearly * 0.01M;
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly-yearlyTax)/12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+
+                    }
+                    else if(yearly> firstLimitMa && yearly <= secondLimitMa)
+                    {
+                        remainingAmount = yearly - firstLimitMa;
+                        yearlyTax = (remainingAmount * 0.15M)+ (yearly * 0.01M);
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly - yearlyTax) / 12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+                    }
+                    else if(yearly> secondLimitMa /*&& yearly <= thirdLimitMa*/)
+                    {
+                        remainingAmount = yearly - firstLimitMa;
+                        secondRemainingAmount = remainingAmount - (secondLimitMa - firstLimitMa);
+
+                        yearlyTax = (secondRemainingAmount*0.25M)+(remainingAmount * 0.15M) + (yearly * 0.01M);
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly - yearlyTax) / 12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    if (yearly > 0 && yearly <= firstLimitUn)
+                    {
+                        yearlyTax = yearly * 0.01M;
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly - yearlyTax) / 12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+                    }
+                    else if (yearly > firstLimitUn && yearly <= secondLimitUn)
+                    {
+                        remainingAmount = yearly - firstLimitUn;
+                        yearlyTax = (remainingAmount * 0.15M) + (yearly * 0.01M);
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly - yearlyTax) / 12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+                    }
+                    else if (yearly > secondLimitUn)
+                    {
+                        remainingAmount = yearly - firstLimitUn;
+                        secondRemainingAmount = remainingAmount - (secondLimitMa - firstLimitMa);
+
+                        yearlyTax = (secondRemainingAmount * 0.25M) + (remainingAmount * 0.15M) + (yearly * 0.01M);
+                        monthlyTax = yearlyTax / 12;
+                        monthlyAfterTaxDeduction = (yearly - yearlyTax) / 12;
+                        monthlyAfterTaxDeduction = monthly - monthlyTax;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                // return "Value Cannot be Null";
+            }
+
+            monthlyAfterTaxDeduction = decimal.Round(monthlyAfterTaxDeduction,2);
+            return monthlyAfterTaxDeduction.ToString();
+        }
+    }
+}
---
+++ Taxation/Taxation/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Text;
+using System;
+using Android.Views;
+
+namespace Taxation
+{
+    [Activity(Label = "Taxation", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Get our UI controls from the loaded layout
+            EditText monthlySalary = FindViewById<EditText>(Resource.Id.Monthly);
+            TextView Result = FindViewById<TextView>(Resource.Id.Result);
+            Button Calculate = FindViewById<Button>(Resource.Id.btn_calc);
+            CheckBox isMarried = FindViewById<CheckBox>(Resource.Id.isMarried);
+            CheckBox isBonus = FindViewById<CheckBox>(Resource.Id.isYearlyBonus);
+            EditText BonusAmount = FindViewById<EditText>(Resource.Id.BonusAmount);
+
+            //var layout = new LinearLayout(this);
+            //layout.Orientation = Orientation.Vertical;
+
+            //EditText BonusAmount = new EditText(this);
+
+            //BonusAmount.Text = "Enter Value";
+
+
+
+            isBonus.Click += (sender, e) =>
+            {
+                if (isBonus.Checked)
+                {
+                    //0 is visible and 4 is invisible an 8 is gone
+                    BonusAmount.Visibility = 0;
+
+                    BonusAmount.Text = "Enter Value";
+                    //layout.AddView(BonusAmount);
+                }
+                else
+                {
+                    BonusAmount.Visibility = (ViewStates)4;
+                }
+            };
+
+            
+            //layout.AddView(aButton);
+           // SetContentView(layout);
+
+            //isBonus.Click += (sender, e) =>
+            // {
+            //     if (isBonus.Checked)
+            //     {
+            //         View view = new View();
+
+            //         Resource.Layout.Main
+
+
+
+            // }
+
+            // };
+
+            //    public override View GetView()
+            //{
+
+            //};
+
+
+            Calculate.Click += (sender, e) =>
+              {
+                  if (string.IsNullOrWhiteSpace(monthlySalary.Text))
+                  {
+                      Result.Text = "Enter Valid data. For E.g. 10000";
+                  }
+
+                  else
+                  {
+                      // decimal monthly = converter.ToDecimal(monthlySalary.Text);
+                      Result.Text = core.Calculator.Calculate(monthlySalary.Text, isMarried.Checked);
+                  }
+              };
+        }
+    }
+}
+
